45e4b764accb9b05f8ea50ac7a395d3218711eab
Prajjwal Goswami <mr.prajjwalji@gmail.com>
2024-02-21 23:33:08 +0530

    learn by doing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,22 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: ["./dist/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // you can either spread `colors` to apply all the colors
+        ...colors,
+        // or add them one by one and name whatever you want
+        sky: colors.lightBlue, // Replace lightBlue with sky
+        stone: colors.warmGray, // Replace warmGray with stone
+        neutral: colors.trueGray, // Replace trueGray with neutral
+        gray: colors.coolGray, // Replace coolGray with gray
+        slate: colors.blueGray, // Replace blueGray with slate
+    },
   },
   plugins: [],
 }
-
+}
